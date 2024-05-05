8bfbe1e124d230c74bb184ca074592540d54cfd0
Muddusar Zulfiqar <muddusarzulfiqar@gmail.com>
2024-05-06 00:14:52 +0500

    enable react-query

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import {fileURLToPath, URL} from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // alias set
-  resolve: {
-    alias: {
-      "@": "/src",
+    plugins: [react()],
+    // alias set
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
-  // proxy set
-  server: {
-    port: 8080,
-  },
+    // proxy set
+    server: {
+        port: 8080,
+    },
 
-  // scss variables set
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/scss/utility/_variables.scss";`,
-      },
+    // scss variables set
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/scss/utility/_variables.scss";`,
+            },
+        },
     },
-  },
+
 });
