bcb8991ec7073425b0a3ef947c12a8ba98a8ab74
Muddusar Zulfiqar <75011439+MuddusarZulfiqar@users.noreply.github.com>
2025-05-26 03:18:36 +0500

    add required dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { RouterProvider } from 'react-router'
 import { router } from '@/routes'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <>
       <RouterProvider router={router} />
+      <Toaster
+        position="bottom-center"
+        // add duration global
+        toastOptions={{
+          duration:4000
+        }}
+      />
     </>
   )
 }
