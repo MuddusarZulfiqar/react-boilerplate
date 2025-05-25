6543824a238f078dd97dca661126d948a5fef373
Muddusar Zulfiqar <75011439+MuddusarZulfiqar@users.noreply.github.com>
2025-05-26 03:05:06 +0500

    update types env

--- src/constants/env.ts
+++
@@ -1,3 +0,0 @@
-const env = {
-    data: import.meta.VITE_
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,8 @@ interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string;
     readonly VITE_APP_ENV: 'development' | 'production';
 }
+
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
