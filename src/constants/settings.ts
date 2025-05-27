710f77e47bebc4037a32c32149ac85abcb419f42
Muddusar Zulfiqar <75011439+MuddusarZulfiqar@users.noreply.github.com>
2025-05-28 01:42:30 +0500

    Update settings.ts

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -3,6 +3,6 @@ import { Settings } from "@/types";
 export const settings:Settings = {
     appName: 'MyApp',
     version: '1.0.0',
-    apiWaitTime: 8000, // 5 seconds
     favIcon: '/favicon.ico',
+    apiWaitTime: 8000, // 5 seconds
 } as const;
